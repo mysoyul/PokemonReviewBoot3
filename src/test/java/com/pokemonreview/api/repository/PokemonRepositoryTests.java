011b5ea7364d785f797e40be31e9b1e93b0331f8
mysoyul <vega2k_@naver.com>
2023-07-24 13:12:05 +0900

    first commit

---
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -0,0 +1,36 @@
+package com.pokemonreview.api.repository;
+
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.PokemonType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+class PokemonRepositoryTests {
+
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @Test
+    public void PokemonRepository_SaveAll_ReturnSavedPokemon() {
+        //Arrange
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC)
+                .build();
+        //Act
+        Pokemon savedPokemon = pokemonRepository.save(pokemon);
+
+        //Assert
+        assertThat(savedPokemon).isNotNull();
+        assertThat(savedPokemon.getId()).isGreaterThan(0);
+    }
+}
