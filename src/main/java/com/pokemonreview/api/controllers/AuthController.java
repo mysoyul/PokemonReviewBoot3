0257c4473c05dc1601be244eecf02bbc4b4fe62d
mysoyul <vega2k_@naver.com>
2023-07-25 10:19:33 +0900

    authcontroller login add

--- src/main/java/com/pokemonreview/api/controllers/AuthController.java
+++ src/main/java/com/pokemonreview/api/controllers/AuthController.java
@@ -1,14 +1,21 @@
 package com.pokemonreview.api.controllers;
 
+import com.pokemonreview.api.dto.AuthResponseDto;
+import com.pokemonreview.api.dto.LoginDto;
 import com.pokemonreview.api.dto.UserDto;
 import com.pokemonreview.api.models.Role;
 import com.pokemonreview.api.models.UserEntity;
 import com.pokemonreview.api.repository.RoleRepository;
 import com.pokemonreview.api.repository.UserRepository;
 
+import com.pokemonreview.api.security.JWTGenerator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,18 +28,28 @@ public class AuthController {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
-
-//		public AuthController(UserRepository userRepository,
-//                          RoleRepository roleRepository,
-//                          PasswordEncoder passwordEncoder) {
-//        this.userRepository = userRepository;
-//        this.roleRepository = roleRepository;
-//        this.passwordEncoder = passwordEncoder;
-//    }
+    private final AuthenticationManager authenticationManager;
+    private final JWTGenerator jwtGenerator;
 
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@RequestBody LoginDto loginDto){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(),
+                        loginDto.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerator.generateToken(authentication);
+        AuthResponseDto authResponseDTO = new AuthResponseDto(token);
+        authResponseDTO.setUsername(loginDto.getUsername());
+        UserEntity userEntity = userRepository.findByUsername(loginDto.getUsername()).get();
+        authResponseDTO.setRole(userEntity.getRoles().get(0).getName());
+        return new ResponseEntity<>(authResponseDTO, HttpStatus.OK);
     }
 
     @PostMapping("/register")
