88e10f0116fb13e7a8efa95de0ba0a67f8c019f9
mysoyul <vega2k_@naver.com>
2023-07-25 10:11:32 +0900

    securityconfig update

--- src/main/java/com/pokemonreview/api/security/SecurityConfig.java
+++ src/main/java/com/pokemonreview/api/security/SecurityConfig.java
@@ -1,7 +1,9 @@
-package com.pokemonreview.api.config;
+package com.pokemonreview.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +13,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private CustomUserDetailsService userDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/com/pokemonreview/api/security/SecurityConstants.java
+++ src/main/java/com/pokemonreview/api/security/SecurityConstants.java
@@ -1,5 +1,5 @@
 package com.pokemonreview.api.security;
 
 public class SecurityConstants {
-    public static final long JWT_EXPIRATION = 86400000;
+    public static final long JWT_EXPIRATION = 86400000;  //24시간
 }
